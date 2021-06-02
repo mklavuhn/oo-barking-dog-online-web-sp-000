b655e47e5f5b808341c14bc3b21e51154b8bdc8d
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-02 20:20:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @name_of_dog = dog_name
+  end
+  
+  def name 
+    @name_of_dog
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
